fd1cd37ee7ec3898a849425196382822b9ccd312
김종혁 <enfycius@gimjonghyeog-ui-MacBookPro.local>
2023-11-02 22:31:12 +0900

    Tutorials: Exercise #3

--- Tutorials/Forward List/forward_list.cpp
+++ Tutorials/Forward List/forward_list.cpp
@@ -1,43 +1,43 @@
+////
+//// Created by 김종혁 on 2023/10/31.
+////
 //
-// Created by 김종혁 on 2023/10/31.
+//#include <forward_list>
+//#include <iostream>
 //
-
-#include <forward_list>
-#include <iostream>
-
-using namespace std;
-
-int main() {
-    forward_list<int> flist1;
-    forward_list<int> flist2;
-    forward_list<int> flist3;
-
-    forward_list<int> flist_emplace1;
-
-    flist1.assign({1, 2, 3});
-    flist2.assign(5, 10);
-    flist3.assign(flist1.begin(), flist1.end());
-
-    for (int& a : flist1)
-        cout << a << " ";
-
-    cout << endl;
-
-    for (int& b : flist2)
-        cout << b << " ";
-
-    cout << endl;
-
-    for (int& c : flist3)
-        cout << c << " ";
-
-    cout << endl;
-
-    flist_emplace1.emplace_front(1);
-    flist_emplace1.emplace_front(100);
-
-    for (int& a : flist_emplace1)
-        cout << a << " ";
-
-    return 0;
-}
+//using namespace std;
+//
+//int main() {
+//    forward_list<int> flist1;
+//    forward_list<int> flist2;
+//    forward_list<int> flist3;
+//
+//    forward_list<int> flist_emplace1;
+//
+//    flist1.assign({1, 2, 3});
+//    flist2.assign(5, 10);
+//    flist3.assign(flist1.begin(), flist1.end());
+//
+//    for (int& a : flist1)
+//        cout << a << " ";
+//
+//    cout << endl;
+//
+//    for (int& b : flist2)
+//        cout << b << " ";
+//
+//    cout << endl;
+//
+//    for (int& c : flist3)
+//        cout << c << " ";
+//
+//    cout << endl;
+//
+//    flist_emplace1.emplace_front(1);
+//    flist_emplace1.emplace_front(100);
+//
+//    for (int& a : flist_emplace1)
+//        cout << a << " ";
+//
+//    return 0;
+//}
